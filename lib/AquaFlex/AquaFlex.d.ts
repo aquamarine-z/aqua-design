383ab70255526dd2262544ab831864dea485fda7
24180 <2418013844@qq.com>
2024-06-12 21:29:01 +0800

    update

---
+++ lib/AquaFlex/AquaFlex.d.ts
@@ -0,0 +1,14 @@
+import React from "react";
+export declare function AquaFlex(props: {
+    children: React.ReactNode[];
+    flexDirection?: "row" | "column";
+    justifyContent?: "flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "space-evenly";
+    alignItems?: "flex-start" | "flex-end" | "center" | "stretch" | "baseline";
+}): import("react/jsx-runtime").JSX.Element;
+export declare namespace AquaFlex {
+    var defaultProps: {
+        flexDirection: string;
+        justifyContent: string;
+        alignItems: string;
+    };
+}
