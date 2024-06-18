cd24220adb8178499776fe2fd123e24a7edee069
24180 <2418013844@qq.com>
2024-06-18 13:49:38 +0800

    add the Component: AqDraggable,AqModalDialog add the Theme: AqGlassTheme

--- packages/index.ts
+++ packages/index.ts
@@ -3,10 +3,4 @@ export {AqColumn,AqFlex,AqRow,AqModalDialog,AqDraggable}
 
 import {AqGlassTheme} from "./themes";
 export {AqGlassTheme}
-
-import "./components/AqDraggable/AqDraggable.module.less"
-import "./components/AqModalDialog/AqModalDialog.module.less"
-import "./components/AqRow/AqRow.module.less"
-import "./components/AqColumn/AqColumn.module.less"
-import "./components/AqFlex/AqFlex.module.less"
-import "./themes/AqGlassTheme/AqGlassTheme.module.less"
+import "./style.css"
