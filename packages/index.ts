6ae2550d982295f3f0cb6774451c2940a8957d4c
24180 <2418013844@qq.com>
2024-06-24 10:21:57 +0800

    add the Component: AqDraggable,AqModalDialog add the Theme: AqGlassTheme

--- packages/index.ts
+++ packages/index.ts
@@ -9,11 +9,3 @@ const css =import.meta.glob("./style.css",{eager:true})
 export {
     less,css
 }
-
-import "./components/AqColumn/AqColumn.module.less"
-import "./components/AqRow/AqRow.module.less"
-import "./components/AqFlex/AqFlex.module.less"
-import "./components/AqModalDialog/AqModalDialog.module.less"
-import "./components/AqDraggable/AqDraggable.module.less"
-import "./themes/AqGlassTheme/AqGlassTheme.module.less"
-import "./style.css"
